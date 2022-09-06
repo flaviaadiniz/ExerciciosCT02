40cbab943438d8e885c79616db355fa4028fb77f
flaviaadiniz <flaviaadiniz@gmail.com>
2022-09-06 14:23:04 -0300

    Adicionar enunciado da atividade.

--- src/ParOuImpar.java
+++ src/ParOuImpar.java
@@ -1,3 +1,8 @@
+/*
+Enunciado: Criar um código para que o usuário digite um número no
+terminal e ele retorne dizendo se o número é par ou ímpar.
+ */
+
 public class ParOuImpar {
 
     public static void main(String[] args) {
